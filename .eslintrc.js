9331da966db07c63dbf9ecc5b86829ba0ff6fc16
ajanisius <ajanisius@gmail.com>
2024-09-27 20:05:19 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+        'plugin:vue/vue3-recommended',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        // Add any additional rules you want here
+    }
+};
---
+++ resources/js/app.ts
@@ -0,0 +1,23 @@
+import './bootstrap';
+import '../css/app.css';
+
+import type { DefineComponent } from 'vue';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+
+createInertiaApp({
+    title: (title) => `f0rum ${title ? ` | ${title}`: ''}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#207eff',
+    },
+});
