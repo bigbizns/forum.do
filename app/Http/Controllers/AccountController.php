0fc055892c00ef91926e7aef46905f8e98e1c5c4
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-10-02 22:38:38 +0300

    FOR-17: Profile user info
    - Fetched User from DB.
    - Implemented user's info from db.
    - Updated tsconfig.
    - Added UserInterface.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -11,9 +12,14 @@ class AccountController extends Controller
 {
     public function __invoke(): Response
     {
-        return Inertia::render('Account/Profile');
+        $user = Auth::user()->only('username', 'email');
+        $user['created_at'] = Auth::user()->created_at->format('d-m-Y');
+
+
+        return Inertia::render('Account/Profile', ['userData' => $user]);
     }
-    public function settings():Response
+
+    public function settings(): Response
     {
         return Inertia::render('Account/Settings');
     }
