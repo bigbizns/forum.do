fccc24abe651105a3b488182f6a557f4418130ad
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-10-03 10:28:06 +0300

    FOR-13: Login/Register-components
    - Created reusable components.
    - Added reusable components.

---
+++ resources/js/Components/FormInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    modelValue: string | null,
+    label: string,
+    labelName: string,
+    type: string,
+    error: string,
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+<template>
+    <div class="mb-4">
+        <label :for="label" class="block text-gray-700 text-sm font-medium mb-1">{{ labelName }}</label>
+        <input
+            :id="label"
+            :type="type"
+            :value="modelValue"
+            @input="emit('update:modelValue', $event.target.value)"
+            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            :class="{'border-red-600': error}"
+        />
+        <small class="text-red-600 font-semibold">{{ error }}</small>
+    </div>
+</template>
---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+    type: string
+    text: string
+}>();
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="bg-blue-500 text-white font-semibold py-2 px-8 rounded-lg hover:bg-blue-600 transition duration-300">
+        {{ text }}
+    </button>
+</template>
