d53a44fce850213d283a991de2ac9b3430b6599b
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-10-09 17:49:38 +0300

    FOR-15: New Font & Icons for Page
    - Added new font.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
     @vite('resources/js/app.ts')
     @inertiaHead
 </head>
-<body class="bg-gray-700">
+<body class="bg-gray-700 font-Nunito">
 @inertia
 @routes
 </body>
