56f026fe4cf0341e0aceba805fd7fb27dd4fd1b0
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-10-15 18:49:56 +0300

    FOR-32: Recover page Components
    - Replaced input with reusable input.

--- resources/js/Pages/Auth/UpdatePassword.vue
+++ resources/js/Pages/Auth/UpdatePassword.vue
@@ -3,6 +3,7 @@ import {computed, ref} from "vue";
 import {Head, useForm} from "@inertiajs/vue3";
 import openEye from '@/images/open-eye.png';
 import closeEye from '@/images/close-eye.png';
+import FormInputPassword from "@/Components/FormInputPassword.vue";
 
 const passwordToText = ref<boolean>(false);
 const confirmPasswordToText = ref<boolean>(false);
@@ -44,34 +45,8 @@ const submit = () => {
                 <h1 class="text-3xl font-bold text-white mb-6">
                     Update your password
                 </h1>
-                <div class="mb-4">
-                    <label for="password" class="block text-white text-sm font-medium mb-1">New Password</label>
-                    <div class="flex items-center border border-gray-300 bg-white rounded-lg">
-                        <input
-                            v-model="form.password"
-                            id="password"
-                            class="w-full px-4 py-2 rounded-l-lg"
-                            :type="passwordToText ? 'text': 'password'"
-                        />
-                        <img :src="passwordEyeIcon" alt="" class="w-4 mx-4 cursor-pointer"
-                             @click="togglePasswordToText">
-                    </div>
-                    <small class="text-red-500 font-semibold">{{ form.errors.password }}</small>
-                </div>
-                <div class="mb-4">
-                    <label for="password_confirmation" class="block text-white text-sm font-medium mb-1">Confirm New
-                        Password</label>
-                    <div class="flex items-center border border-gray-300 bg-white rounded-lg">
-                        <input
-                            v-model="form.password_confirmation"
-                            id="password_confirmation"
-                            class="w-full px-4 py-2 rounded-l-lg"
-                            :type="confirmPasswordToText ? 'text': 'password'"
-                        />
-                        <img :src="confirmPasswordEyeIcon" alt="" class="w-4 mx-4 cursor-pointer"
-                             @click="toggleConfirmPasswordToText">
-                    </div>
-                </div>
+                <FormInputPassword v-model="form.password" label="password" label-name="New Password" :error="form.errors.password"/>
+                <FormInputPassword v-model="form.password_confirmation" label="password_confirmation" label-name="Confirm New Password"/>
                 <div class="flex justify-center">
                     <button
                         type="submit"
