c8feb32ba2a1cbe07441efd755e955a8a1391d92
ajanisius <ajanisius@gmail.com>
2024-10-17 15:42:15 +0300

    FOR-64: User Profile UI update
    - Updated backgrounds while hovering.

--- resources/js/Pages/UserAccount/Components/ActivityCard.vue
+++ resources/js/Pages/UserAccount/Components/ActivityCard.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="bg-black/40 p-4 rounded-lg shadow-sm cursor-pointer">
+    <div class="bg-black/40 p-4 rounded-lg shadow-sm cursor-pointer transition duration-200 hover:bg-black/20">
         <p class="font-medium text-gray-300">Posted in <b href="#" class="hover:underline">Discussion Topic</b></p>
         <p class="text-sm text-gray-500">2 hours ago</p>
     </div>
