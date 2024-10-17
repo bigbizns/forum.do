4615754ae7b5c0bde3b0c8c4ea8f1f146268f36a
ajanisius <ajanisius@gmail.com>
2024-10-17 15:51:04 +0300

    FOR-65: All posts data
    - Replaced with real ones posts.
    - Added pagination like in home.

--- resources/js/Pages/Posts/Index/ForumIndex.vue
+++ resources/js/Pages/Posts/Index/ForumIndex.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import Navigation from "@/Layouts/Navigation.vue";
 import Footer from "@/Layouts/Footer.vue";
-import {Head} from "@inertiajs/vue3";
+import {Head, Link} from "@inertiajs/vue3";
 import Post from "@/Components/Post.vue";
+
+const props = defineProps<{
+    posts: {
+        data: PostInterface[],
+        pagination: PaginationInterface
+    };
+}>();
 </script>
 
 <template>
@@ -11,9 +18,34 @@ import Post from "@/Components/Post.vue";
     <div class="mt-20 container h-screen mx-auto p-6">
         <div class="bg-black/40 flex flex-col gap-4 rounded-lg p-4 mt-2">
             <h1 class="text-3xl font-bold text-white mb-6">All Forum Posts</h1>
-            <Post :user_id="2" description="NEED BOOST" :id="1" avatar="" title="FORTNITE"/>
-            <Post :user_id="2" description="SELLING BOOST" :id="1" avatar="" title="CS2"/>
-            <Post :user_id="2" description="BUYING ACCOUNT" :id="1" avatar="" title="DOTA2"/>
+            <Post
+                v-for="post in posts.data"
+                :trade-action="post.tradeAction"
+                :key="post.title"
+                :avatar="post.avatar"
+                :title="post.title"
+                :id="post.id"
+                :description="post.description"
+                :user_id="post.user_id"/>
+        </div>
+        <div class="flex items-center justify-end gap-5 mt-4">
+            <Link
+                preserve-scroll
+                v-if="props.posts.pagination.prev_page_url"
+                :href="props.posts.pagination.prev_page_url"
+                class="text-white px-4 py-2 bg-gray-800 rounded transition duration-200 hover:bg-gray-500">
+                Previous
+            </Link>
+            <Link
+                preserve-scroll
+                v-if="props.posts.pagination.next_page_url"
+                :href="props.posts.pagination.next_page_url"
+                class="text-white px-4 py-2 bg-gray-800 rounded transition duration-200 hover:bg-gray-500">
+                Next
+            </Link>
+            <p class="text-white text-sm">
+                Page {{ props.posts.pagination.current_page }} of {{ props.posts.pagination.last_page }}
+            </p>
         </div>
     </div>
     <Footer/>
