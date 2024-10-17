87d42cdc19dfdbefe5ef790c80e89c5d3304cd4d
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-10-17 20:35:09 +0300

    FOR-65: All posts data
    - Replaced with real ones posts.
    - Added pagination like in home.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,10 +19,12 @@ class PostController extends Controller
 {
     public function __invoke(): Response
     {
-        return Inertia::render('Posts/Index/ForumIndex');
+        $posts = $this->getPaginatedPosts();
+
+        return Inertia::render('Posts/Index/ForumIndex', ['posts' => $posts]);
     }
 
-    public function showPost(int $id):Response
+    public function showPost(int $id): Response
     {
         $post = $this->getPostInfo($id);
         $comments = $this->getComments($id);
@@ -32,10 +34,10 @@ class PostController extends Controller
 
     public function create(): Response
     {
-        $categories = Category::all(['id','title', 'marketplace']);
+        $categories = Category::all(['id', 'title', 'marketplace']);
         $tradeActions = tradeActionEnum::getTradeActions();
 
-        return Inertia::render('Posts/Create/CreatePost', ['categories' => $categories , 'tradeActions' => $tradeActions]);
+        return Inertia::render('Posts/Create/CreatePost', ['categories' => $categories, 'tradeActions' => $tradeActions]);
     }
 
     public function store(StorePost $request): RedirectResponse
@@ -99,5 +101,37 @@ class PostController extends Controller
         }
 
         return $comments;
+    }
+
+    private function getPaginatedPosts(): array
+    {
+        $recentPosts = Post::orderBy('created_at', 'desc')->paginate(10);
+
+        $posts = [];
+
+        foreach ($recentPosts as $recentPost) {
+            $posts[] = [
+                'id' => $recentPost->id,
+                'user_id' => $recentPost->user_id,
+                'title' => $recentPost->title,
+                'tradeAction' => $recentPost->tradeAction,
+                'description' => $recentPost->description,
+                'created_at' => $recentPost->created_at->format('d-m-Y'),
+                'avatar' => $recentPost->user->avatar,
+            ];
+        }
+
+        return [
+            'data' => $posts,
+            'pagination' => [
+                'links' => $recentPosts->linkCollection(),
+                'current_page' => $recentPosts->currentPage(),
+                'last_page' => $recentPosts->lastPage(),
+                'per_page' => $recentPosts->perPage(),
+                'total' => $recentPosts->total(),
+                'next_page_url' => $recentPosts->nextPageUrl(),
+                'prev_page_url' => $recentPosts->previousPageUrl(),
+            ],
+        ];
     }
 }
