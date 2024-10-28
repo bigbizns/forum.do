b958cc388dc7f45091d79854a10cd9ad89240c54
ajanisius <ajanisius@gmail.com>
2024-10-28 17:34:34 +0200

    FOR-10: Page's Favicon
    - Created/Added favicon.
    - Added Meta tags for better SEO.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,18 @@
 <html>
 <head>
     <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="title" content="forum.do">
+    <meta name="description" content="Join our forum to discuss gaming, buy/sell accounts, share tips">
+    <meta name="keywords" content="forum, marketplace, social accounts, gaming accounts, buy sell trade, online marketplace, gaming discussions, account marketplace, game forums, social media trading, buy gaming accounts, sell social accounts, community forum, gaming community, account exchange, gaming marketplace, account selling, account buying, digital marketplace, social account marketplace, game trading, gamer chat, digital goods, social network trading, gaming services, online forum, virtual assets, game account trading, tech community, gaming news, gaming tips, account sales, virtual marketplace, buy accounts, sell accounts">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
