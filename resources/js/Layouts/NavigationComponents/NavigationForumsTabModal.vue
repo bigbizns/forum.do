a6bfb28f38595b7d4df2fa71a80bae2bc7939b1f
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-11-06 15:47:33 +0200

    FOR-85: UI Update in navigation's dropdown
    - Changed Colors of dropdowns.

--- resources/js/Layouts/NavigationComponents/NavigationForumsTabModal.vue
+++ resources/js/Layouts/NavigationComponents/NavigationForumsTabModal.vue
@@ -3,7 +3,7 @@ import {Link} from "@inertiajs/vue3";
 </script>
 
 <template>
-    <div class="flex flex-col text-black absolute top-14 right-50 bg-white w-40 rounded-lg shadow-lg p-2 border border-gray-200 z-50">
+    <div class="flex flex-col text-white absolute top-14 right-50 bg-black/60 w-40 rounded-lg shadow-lg p-2 border border-gray-600 z-50">
         <Link :href="route('forum')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">All Forum posts</Link>
         <Link :href="route('post.create')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">Create Post</Link>
     </div>
