78a75d37b4bbb39f59a8dd86f6e26b6c1c72c211
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-11-07 11:40:33 +0200

    FOR-80: Home view categories navigation Update
    - Fetched Categories from DB.
    - Added npm carousel for UI.
    - Seperated Link to new component.

---
+++ resources/js/Pages/Home/HomeComponents/TopicLink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {Link} from "@inertiajs/vue3";
+
+defineProps<{
+    title: string
+}>();
+</script>
+
+<template>
+    <Link href=""
+          class="px-8 py-4 m-2 hover:bg-gray-700 transition duration-300 text-opacity text-white rounded text-gray">
+        {{ title }}
+    </Link>
+</template>
