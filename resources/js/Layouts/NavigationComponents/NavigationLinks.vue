31c371e16f16f1e04ae1b9f97a05ec15c740d3da
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-11-19 14:02:48 +0200

    FOR-104: Main navigation upgrades
    - Added to nav create post.

--- resources/js/Layouts/NavigationComponents/NavigationLinks.vue
+++ resources/js/Layouts/NavigationComponents/NavigationLinks.vue
@@ -15,7 +15,7 @@ const toggleForumModal = () => {
             <Link :href="route('home')" class="duration-300 transition hover:text-blue-500">Home</Link>
         </li>
         <li>
-            <div @click="toggleForumModal" class="flex items-center duration-300 transition cursor-pointer hover:text-blue-500 group">Forums
+            <div @click="toggleForumModal" class="flex items-center duration-300 transition cursor-pointer hover:text-blue-500 group">Forum
                 <svg class="w-3 ml-2 group-hover:fill-blue-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M12 15l-8-8h16z"/></svg>
             </div>
         </li>
@@ -23,6 +23,10 @@ const toggleForumModal = () => {
             <NavigationForumsTabModal/>
         </template>
         <li>
+        <Link :href="route('post.create')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">Create Post</Link>
+        </li>
+        <li>
+
             <Link :href="route('contactus')" class="duration-300 transition hover:text-blue-500">Contact</Link>
         </li>
     </ul>
