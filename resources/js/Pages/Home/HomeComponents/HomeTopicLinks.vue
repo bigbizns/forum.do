f3cfbed95fbdf20b2acf1b4bb659b6dccb79936f
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-12-02 18:36:31 +0200

    FOR-117: Home carousel view
    - Fixed mobile screen view.

--- resources/js/Pages/Home/HomeComponents/HomeTopicLinks.vue
+++ resources/js/Pages/Home/HomeComponents/HomeTopicLinks.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts">
 import TopicLink from "@/Pages/Home/HomeComponents/TopicLink.vue";
-import 'vue3-carousel/dist/carousel.css'
-import {Carousel, Slide, Navigation} from 'vue3-carousel'
-import {CategoryInterface} from "@/Types/CategoryInterface";
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+import { CategoryInterface } from "@/Types/CategoryInterface";
 
 defineProps<{
     categories: CategoryInterface[]
 }>();
-
-const config = {
-    itemsToShow: 5,
-    wrapAround: true,
-};
 </script>
 
 <template>
     <div class="flex items-center rounded-lg">
-        <Carousel v-bind="config" class="w-full">
-            <Slide v-for="category in categories" :key="category.id">
-                <TopicLink :title="category.title" :slug="category.slug"/>
+        <Carousel
+            :itemsToShow="5"
+            class="hidden md:block w-full"
+            wrapAround>
+            <Slide
+                v-for="category in categories"
+                :key="category.id"
+                class="px-2">
+                <TopicLink :title="category.title" :slug="category.slug" />
             </Slide>
             <template #addons>
-                <Navigation/>
+                <Navigation />
+            </template>
+        </Carousel>
+
+        <Carousel
+            :itemsToShow="1"
+            class="md:hidden w-full"
+            wrapAround>
+            <Slide
+                v-for="category in categories"
+                :key="category.id"
+                class="px-2">
+                <TopicLink :title="category.title" :slug="category.slug" />
+            </Slide>
+            <template #addons>
+                <Navigation />
             </template>
         </Carousel>
     </div>
