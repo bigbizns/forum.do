7209c8e61c93885fc441c238d1227e15e9f9edc6
ajanisius <116707198+ajanisius@users.noreply.github.com>
2024-12-03 16:54:37 +0200

    FOR-124: Navigation dropdown
    - Installed new NPM package - Headless UI.
    - Replaced Modal Drop-offs.

--- resources/js/Layouts/NavigationComponents/NavigationForumsTabModal.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import {Link} from "@inertiajs/vue3";
-</script>
-
-<template>
-    <div class="flex flex-col text-white absolute top-14 right-50 bg-black/60 w-40 rounded-lg shadow-lg p-2 border border-gray-600 z-50">
-        <Link :href="route('forum')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">All Forum posts</Link>
-    </div>
-</template>
--- resources/js/Layouts/NavigationComponents/NavigationLinks.vue
+++ resources/js/Layouts/NavigationComponents/NavigationLinks.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
-import {ref} from "vue";
 import {Link} from "@inertiajs/vue3";
-import NavigationForumsTabModal from "@/Layouts/NavigationComponents/NavigationForumsTabModal.vue";
-
-const forumsModalOpen = ref<boolean>(false);
-const toggleForumModal = () => {
-    forumsModalOpen.value = !forumsModalOpen.value;
-};
+import {Popover, PopoverButton, PopoverPanel} from "@headlessui/vue";
 </script>
 
 <template>
@@ -14,24 +8,35 @@ const toggleForumModal = () => {
         <li>
             <Link :href="route('home')" class="duration-300 transition hover:text-blue-500">Home</Link>
         </li>
-        <li>
-            <div @click="toggleForumModal" class="flex items-center duration-300 transition cursor-pointer hover:text-blue-500 group">Forum
-                <svg class="w-3 ml-2 group-hover:fill-blue-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M12 15l-8-8h16z"/></svg>
+            <div class="max-w-sm">
+                <Popover v-slot="{ open }" class="relative">
+                    <PopoverButton :class="open ? 'text-white' : 'text-white/90'" class="group inline-flex items-center rounded-md text-base font-medium transition duration-300 hover:text-blue-500">
+                        Forum
+                    </PopoverButton>
+                    <transition
+                        enter-active-class="transition duration-200 ease-out"
+                        enter-from-class="translate-y-1 opacity-0"
+                        enter-to-class="translate-y-0 opacity-100"
+                        leave-active-class="transition duration-150 ease-in"
+                        leave-from-class="translate-y-0 opacity-100"
+                        leave-to-class="translate-y-1 opacity-0"
+                    >
+                        <PopoverPanel
+                            class="absolute left-1/2 z-10  w-screen max-w-sm -translate-x-1/2 transform lg:max-w-[20vh]">
+                            <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                                <div class="flex flex-col relative gap-2 bg-black/80 p-2 px-5">
+                                    <Link :href="route('forum')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">All Forum posts</Link>
+                                </div>
+                            </div>
+                        </PopoverPanel>
+                    </transition>
+                </Popover>
             </div>
-        </li>
-        <template v-if="forumsModalOpen">
-            <NavigationForumsTabModal/>
-        </template>
         <li>
         <Link :href="route('post.create')" class="py-2 hover:text-blue-500 w-fit h-fit cursor-pointer transition duration-200 ease-in-out">Create Post</Link>
         </li>
         <li>
-
             <Link :href="route('contactus')" class="duration-300 transition hover:text-blue-500">Contact</Link>
         </li>
     </ul>
 </template>
-
-<style scoped>
-
-</style>
