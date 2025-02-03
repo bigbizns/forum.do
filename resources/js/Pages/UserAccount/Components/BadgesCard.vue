9a7f2765569b037f1167ff8838d1081a8ac5849c
ajanisius <ajanisius@gmail.com>
2025-02-03 10:38:52 +0200

    FOR-140: Show Badge Post Icon
    - Implemented a badge to user profile.

--- resources/js/Pages/UserAccount/Components/BadgesCard.vue
+++ resources/js/Pages/UserAccount/Components/BadgesCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {BadgesEnum} from "@/Enums/BadgesEnum";
 import verifiedEmail from "@/Images/verified_email.png";
+import firstPost from '@/Images/firstPost.png';
 import {BadgesInterface} from "@/Types/BadgesInterface";
 
 defineProps<{
@@ -11,13 +12,18 @@ defineProps<{
 <template>
     <div class="max-w-5xl mx-auto mt-6 bg-black/40 p-6 rounded-lg shadow-md">
         <h3 class="text-gray-500 text-xl font-semibold mb-3">Badges</h3>
-        <template v-if="badges.length != 0" v-for="badge in badges">
-            <div v-if="badge.type === BadgesEnum.EmailVerified">
-                <img :src="verifiedEmail" class="w-14" alt="verified email"/>
-            </div>
-        </template>
-        <template v-else>
-            <p class="text-gray-300 leading-relaxed"> This user has no badges.</p>
-        </template>
+        <div class="flex gap-5 items-center">
+            <template v-if="badges.length != 0" v-for="badge in badges">
+                <div v-if="badge.type === BadgesEnum.EmailVerified">
+                    <img :src="verifiedEmail" class="w-14" alt="verified email"/>
+                </div>
+                <div v-if="badge.type === BadgesEnum.FirstPost">
+                    <img :src="firstPost" class="w-12" alt="first post"/>
+                </div>
+            </template>
+            <template v-else>
+                <p class="text-gray-300 leading-relaxed"> This user has no badges.</p>
+            </template>
+        </div>
     </div>
 </template>
